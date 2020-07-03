1aa7870f2d40b7e31017f621cd6f1cbcd5596719
吉忠晟 <jizs@yunanbao.com.cn>
2020-07-03 23:45:02 +0800

    修复数据不一致问题
    采用单连接复用形式

--- ORM/aioengine.py
+++ ORM/aioengine.py
@@ -1,10 +1,12 @@
 from db_config import *
 from aiomysql.sa import create_engine
 from aiomysql import create_pool
+
 engine = None
+
 
 async def get_engine():
     global engine
     if not engine:
-        engine = await create_engine(user=USERNAME, db=DBNAME, host=HOST, password=PASSWORD)
+        engine = await create_engine(user=USERNAME, db=DBNAME, host=HOST, password=PASSWORD, maxsize=1, pool_recycle=5)
     return engine
